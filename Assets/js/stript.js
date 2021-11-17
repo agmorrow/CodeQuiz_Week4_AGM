cd906f8cd29da7b09eecd8543daa003fe7c1da15
agmorrow <morrow.austin5@gmail.com>
2021-11-16 18:59:39 -0700

    added notes for functions i need to use

--- Assets/js/stript.js
+++ Assets/js/stript.js
@@ -9,12 +9,12 @@ const timeEl = document.querySelector(".timer .countDown");
 
 // if continueQuiz button clicked
 continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.add("activeQuiz"); //show quiz box
-    showQuetions(0); //calling showQestions function
-    queCounter(1); //passing 1 parameter to queCounter
-    startTimer(15); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.add("activeQuiz"); 
+    showQuetions(0); 
+    queCounter(1); 
+    startTimer(15); 
+    startTimerLine(0); 
 }
 
 let timeValue =  60;
@@ -30,11 +30,11 @@ const secondsLeft = 60;
 
 function startTimer() {
     const timerInterval = setInterval(function() {
-        secondsLeft--; //decrement the time value
+        secondsLeft--; 
         timeEl.textContent = secondsLeft + "seconds left!";
 
-        if(secondsLeft == 0) { //if timer is less than 0
-            clearInterval(timerInterval); //clear counter
+        if(secondsLeft == 0) { 
+            clearInterval(timerInterval); 
             sendMessage();        
         }
     }, 1000);
@@ -43,46 +43,35 @@ function startTimer() {
 
 // if startQuiz button clicked
 start_quiz.onclick = ()=>{
-    quiz_box.classList.add("activeQuiz"); //show quiz box
-    result_box.classList.remove("activeResult"); //hide result box
+    quiz_box.classList.add("activeQuiz"); 
+    result_box.classList.remove("activeResult"); 
     timeValue = 60; 
     que_count = 0;
     que_numb = 1;
     userScore = 0;
     widthValue = 0;
-    showQuetions(que_count); //calling showQestions function
-    queCounter(que_numb); //passing que_numb value to queCounter
-    clearInterval(counter); //clear counter
-    clearInterval(counterLine); //clear counterLine
-    startTimer(timeValue); //calling startTimer function
-    startTimerLine(widthValue); //calling startTimerLine function
-    timeText.textContent = "Time Left"; //change the text of timeText to Time Left
-    next_btn.classList.remove("show"); //hide the next button
+    showQuetions(que_count); 
+    queCounter(que_numb); 
+    clearInterval(counter); 
+    clearInterval(counterLine);
+    startTimer(timeValue); 
+    startTimerLine(widthValue); 
+    timeText.textContent = "Time Left"; 
+    next_btn.classList.remove("show"); 
 }
 
 
-
-// getting questions and options from array
-function showQuetions(index){
-    const que_text = document.querySelector(".que_text");
-
-    //creating a new span and div tag for question and option and passing the value using array index
-    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
-    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
-    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
-    
-    const option = option_list.querySelectorAll(".option");
-
-}
+const questions = [
+    { 
+        question: "",
+        options: [],
+        answer: 0,
+    }
+]
 
 
-
-
-
+// localStorage for high scores
+// event.target for what the user actually clicks on
 
 
 
