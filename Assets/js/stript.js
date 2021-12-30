2508e4abe0b10e7ba454402b7aaa1bde2ec5839c
agmorrow <morrow.austin5@gmail.com>
2021-12-29 17:05:14 -0700

    hide function working

--- Assets/js/stript.js
+++ Assets/js/stript.js
@@ -1,91 +1,75 @@
-
-   //selecting all required elements
 const info_box = document.querySelector(".infoBox");
-const start_btn = document.querySelector(".buttons .startQuiz");
+const start_btn = document.getElementById("startQuiz");
+const questionContainerEl = document.getElementById('question-container');
 const questionsEl = document.getElementById("questions");
-const timerEl = document.getElementById("timer");
+// const timerEl = document.getElementById("timer");
 
-let timeLeft = 75;
-const stopTime = 0;
-const subtractTime = 10;
+// let timeLeft = 75;
+// const stopTime = 0;
+// const subtractTime = 10;
 
-const q1Answer1 = document.createElement("button");
-const q1Answer2 = document.createElement("button");
-const q1Answer3 = document.createElement("button");
-const q1Answer4 = document.createElement("button");
+// const questions = [
+//     {
+//         question: "Who was the boss at the Dunder Mifflin Scranton branch?",
+//         choices: ["David Wallace", "Michael Scarn", "Michael Scott", "Jan Levinson"],
+//         answer: 3
+//     }
+// ];
 
-const q2Answer1 = document.createElement("button");
-const q2Answer2 = document.createElement("button");
-const q2Answer3 = document.createElement("button");
-const q2Answer4 = document.createElement("button");
 
-const q3Answer1 = document.createElement("button");
-const q3Answer2 = document.createElement("button");
-const q3Answer3 = document.createElement("button");
-const q3Answer4 = document.createElement("button");
 
-const q4Answer1 = document.createElement("button");
-const q4Answer2 = document.createElement("button");
-const q4Answer3 = document.createElement("button");
-const q4Answer4 = document.createElement("button");
 
-const q5Answer1 = document.createElement("button");
-const q5Answer2 = document.createElement("button");
-const q5Answer3 = document.createElement("button");
-const q5Answer4 = document.createElement("button");
 
-info_box.classList.add("activeInfo"); //show info box
+document.getElementById('question-container').style.display = 'none';
 
-start_btn.addEventListener('click', function() {
-    info_box.style.display = 'none';
-    timer();
-    questions1();
-});
+start_btn.addEventListener('click', startQuiz)
 
-const timeInterval = setInterval(function(){});
-const timer = function() {
-    timeInterval = setInterval(function() {
-        if (timeLeft > 0) {
-            timerEl.textContent = 'Time: ' + timeLeft;
-            timeLeft --;
-        }
+function startQuiz() {
+console.log('Started')
+info_box.style.display = 'none';
+questionContainerEl.classlist.remove('hide')
+//     timer();
+//     questions1();
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer(){
+
+}
+
+
+
+
+// const timeInterval = setInterval(function(){});
+// const timer = function() {
+//     timeInterval = setInterval(function() {
+//         if (timeLeft > 0) {
+//             timerEl.textContent = 'Time: ' + timeLeft;
+//             timeLeft --;
+//         }
     
-    else if (timeLeft <= 0){
-        endQuiz();
-        rightOrWrongEl.remove();
-        q1Answer1.remove();
-        q1Answer2.remove();
-        q1Answer3.remove();
-        q1Answer4.remove();
+//     else if (timeLeft <= 0){
+//         endQuiz();
+     
 
-        q2Answer1.remove();
-        q2Answer2.remove();
-        q2Answer3.remove();
-        q2Answer4.remove();
+//         timerEl.textContent = 'Time: 0';   
+//     }      
+// }, 1000);
+// };
 
-        q3Answer1.remove();
-        q3Answer2.remove();
-        q3Answer3.remove();
-        q3Answer4.remove();
+// const stopTimer = function() {
+//     clearInterval(timeInterval);
+//     timerEl.textContent = "Time: " + timeLeft;
+// };
 
-        q4Answer1.remove();
-        q4Answer2.remove();
-        q4Answer3.remove();
-        q4Answer4.remove();
-
-        q5Answer1.remove();
-        q5Answer2.remove();
-        q5Answer3.remove();
-        q5Answer4.remove();
-
-        timerEl.textContent = 'Time: 0';   
-    }      
-}, 1000);
-};
-
-
-
-
+// const subtractFromTimer = function() {
+//     clearInterval(timeInterval);
+//     timeLeft = timeLeft - subtractTime;
+//     timer();
+// }
 
 
 
