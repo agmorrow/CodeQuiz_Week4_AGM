560a013cd6da950e768d7310b70aae8e536e79f8
agmorrow <morrow.austin5@gmail.com>
2021-12-31 15:38:40 -0700

    next Question working

--- Assets/js/stript.js
+++ Assets/js/stript.js
@@ -1,20 +1,18 @@
 const info_box = document.getElementById("infoBox");
 const start_btn = document.getElementById("startQuiz");
 const questionContainerEl = document.getElementById('question-container');
-const questionsEl = document.getElementById("questions");
+const questionsEl = document.getElementById("question");
+const answerButtonsEl = document.getElementById("answer-button");
+
+let shuffleQuestions, currentQuestionIndex
+
 // const timerEl = document.getElementById("timer");
 
 // let timeLeft = 75;
 // const stopTime = 0;
 // const subtractTime = 10;
 
-// const questions = [
-//     {
-//         question: "Who was the boss at the Dunder Mifflin Scranton branch?",
-//         choices: ["David Wallace", "Michael Scarn", "Michael Scott", "Jan Levinson"],
-//         answer: 3
-//     }
-// ];
+//
 
 
 
@@ -26,19 +24,38 @@ start_btn.addEventListener('click', startQuiz)
 
 function startQuiz() {
     info_box.classList.add('hide')
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerEl.classList.remove('hide')
-
-//     timer();
-//     questions1();
+   setNextQuestion()
 }
 
 function setNextQuestion() {
+   showQuestion(shuffleQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionsEl.innerText = question.question
 }
 
 function selectAnswer(){
 
 }
+
+const questions = [
+        {
+            question: "Who was the boss at the Dunder Mifflin Scranton branch?",
+            choices: ["David Wallace", "Michael Scarn", "Michael Scott", "Jan Levinson"],
+            answer: 3
+        }
+    ];
+
+
+
+
+
+
+
 
 
 
