8c86e432ac6d35cd94ef1ff15447cc59ba055386
agmorrow <morrow.austin5@gmail.com>
2022-01-01 14:50:55 -0700

    timer starts

--- Assets/js/stript.js
+++ Assets/js/stript.js
@@ -3,6 +3,11 @@ const start_btn = document.getElementById("startQuiz");
 const questionContainerEl = document.getElementById('question-container');
 const questionsEl = document.getElementById("question");
 const answerButtonsEl = document.getElementById("answer-button");
+const timerEl = document.getElementById("timer");
+
+let timeLeft = 75;
+const stopTime = 0;
+const subtractTen = 10;
 
 let shuffleQuestions, currentQuestionIndex
 
@@ -13,6 +18,7 @@ info_box.classList.remove('hide')
 start_btn.addEventListener('click', startQuiz)
 
 function startQuiz() {
+    timer()
     info_box.classList.add('hide')
     shuffleQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -46,8 +52,79 @@ function resetState() {
 }
 
 function selectAnswer(e) {
-
+const selectButton = e.target
+const correct = selectButton.dataset.correct
+setStatusClass(document.body, correct)
+Array.from(answerButtonsEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+    })
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+var timeInterval = setInterval(function(){});
+var timer = function(){
+    timeInterval = setInterval(function(){
+        if (timeLeft > 0){
+            timerEl.textContent = 'Time: ' + timeLeft;
+            timeLeft --;
+        }
+        // else if (timeLeft <= 0){
+        //     endQuiz();
+        //     rightOrWrongEl.remove();
+        //     q1Answer1.remove();
+        //     q1Answer2.remove();
+        //     q1Answer3.remove();
+        //     q1Answer4.remove();
+
+        //     q2Answer1.remove();
+        //     q2Answer2.remove();
+        //     q2Answer3.remove();
+        //     q2Answer4.remove();
+
+        //     q3Answer1.remove();
+        //     q3Answer2.remove();
+        //     q3Answer3.remove();
+        //     q3Answer4.remove();
+
+        //     q4Answer1.remove();
+        //     q4Answer2.remove();
+        //     q4Answer3.remove();
+        //     q4Answer4.remove();
+
+        //     q5Answer1.remove();
+        //     q5Answer2.remove();
+        //     q5Answer3.remove();
+        //     q5Answer4.remove();
+
+        //     timerEl.textContent = 'Time: 0';   
+        // }      
+    }, 1000);
+};
+
+var stopTimer = function() {
+    clearInterval(timeInterval);
+    timerEl.textContent = "Time: " + timeLeft;   
+};
+
+var subtractTenFromTimer = function() {
+    clearInterval(timeInterval);
+    timeLeft = timeLeft - subtractTen;
+    timer(); 
+};
+
 
 const questions = [
         {
@@ -60,25 +137,4 @@ const questions = [
             ]
         }
     ]       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
